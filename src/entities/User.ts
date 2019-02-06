b25a34981d75917657ddc6865fc4877df91e508b
Sehwan Choi <sean.sehwan.choi@gmail.com>
2019-02-06 06:14:07 -0500

    Compare password with encrypted password

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -69,6 +69,10 @@ class User extends BaseEntity {
     @CreateDateColumn() createdAt: string
     @UpdateDateColumn() updatedAt: string 
 
+    public comparePassword(password: string = ""):Promise<boolean> {
+        return bcrypt.compare(password, this.password)
+    }
+
     @BeforeInsert()
     @BeforeUpdate()
     async savePassword(): Promise<void> {
